ed55e922164316c361105d5ee27b4703c9d1d11f
Ana Carolina Alves <67441803+anacarolinaalves@users.noreply.github.com>
2025-04-09 20:45:37 -0300

    Laços condicionais - while e do.while

--- JDD01-aula03/src/Media.java
+++ JDD01-aula03/src/Media.java
@@ -1,20 +1,24 @@
 import java.util.Scanner;
 
-public class MediaIdades {
+public class Media {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
+        Scanner leitorTexto = new Scanner(System.in);
         int idade = 0, media, numeroAlunos;
+        String continuar;
         //inicializando a variavel da condição
-        numeroAlunos = 1;
-        while(numeroAlunos <= 5){
-            System.out.println("Digite a idade " + numeroAlunos + "º aluno: ");
+
+        numeroAlunos = 0;
+        do{
+            System.out.println("Digite a idade " + (numeroAlunos+1)+ "º aluno: ");
             idade = idade + leitor.nextInt();
             //Incremento - alterar a variavel da condicao para que em algum momento
             //ela se torne falsa
             numeroAlunos++;
-        }
-        media = idade/5;
+            System.out.println("Deseja informar a idade de outro aluno?  [S/N]");
+            continuar = leitorTexto.nextLine();
+        }while(continuar.equalsIgnoreCase("s"));
+        media = idade/numeroAlunos;
         System.out.println("A média de idade dos alunos da escola é: " + media);
     }
-
 }
--- JDD01-aula03/src/MediaDoWhile.java
+++ JDD01-aula03/src/MediaDoWhile.java
@@ -1,20 +1,19 @@
 import java.util.Scanner;
 
-public class MediaIdades {
+public class MediaDoWhile {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
         int idade = 0, media, numeroAlunos;
         //inicializando a variavel da condição
         numeroAlunos = 1;
-        while(numeroAlunos <= 5){
+        do{
             System.out.println("Digite a idade " + numeroAlunos + "º aluno: ");
             idade = idade + leitor.nextInt();
             //Incremento - alterar a variavel da condicao para que em algum momento
             //ela se torne falsa
             numeroAlunos++;
-        }
+        }while(numeroAlunos <= 5);
         media = idade/5;
         System.out.println("A média de idade dos alunos da escola é: " + media);
     }
-
 }
